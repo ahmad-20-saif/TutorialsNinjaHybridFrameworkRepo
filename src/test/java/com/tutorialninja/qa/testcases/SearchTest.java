e03cc72b49d7b1dcfeb3ec64c8cd3b1caa2306c7
saifa <saifa@Daib>
2024-12-14 16:35:58 +0530

    Added more details.

--- src/test/java/com/tutorialninja/qa/testcases/SearchTest.java
+++ src/test/java/com/tutorialninja/qa/testcases/SearchTest.java
@@ -11,7 +11,7 @@ import com.tutorialsninja.qa.pages.HomePage;
 import com.tutorialsninja.qa.pages.SearchPage;
 
 
-//Updated Comment
+//Updated Comment - Added more details
 public class SearchTest extends Base {
 	
 	SearchPage searchPage ;
